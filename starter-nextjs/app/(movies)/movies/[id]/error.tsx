76c29178e2fd07ad1fd2be1d9eb0eb7553bef575
Xiah <xiah0526@gmail.com>
2024-02-26 11:12:35 +0900

    Nextjs의 data fetching 기능을 학습합니다

---
+++ starter-nextjs/app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function ErrorOMG() {
+    return (
+        <div>
+            <h1>Error - Page Not Found</h1>
+        </div>
+    );
+}
--- starter-nextjs/app/(movies)/movies/[id]/page.tsx
+++ starter-nextjs/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,18 @@
-export default function MovieDetail({params : {id}} : {params: {id: string}}) {
+import MovieVideos from "@/components/movie-videos";
+import MovieInfo from "@/components/movie-info";
+import {Suspense} from "react";
+
+
+export default async function MovieDetail({params : {id}} : {params: {id: string}}) {
     return (
         <div>
-            <h1>Movies NextJS! {id}</h1>
+            <h3>Movie details Page</h3>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
         </div>
     );
 }
